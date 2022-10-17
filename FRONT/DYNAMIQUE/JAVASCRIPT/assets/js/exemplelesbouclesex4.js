348c7ff16a1ab01d031fe67f156b9efc688cd3d7
AfpaYoussef <youssefchaibi@hotmail.com>
2022-10-17 16:26:06 +0000

    terminé

---
+++ FRONT/DYNAMIQUE/JAVASCRIPT/assets/js/exemplelesbouclesex4.js
@@ -0,0 +1,7 @@
+nombre=window.prompt("Entrez un entier:");
+var b =window.prompt("Entrez le nombre de multiples:");
+
+
+for (i = 1; i <= b; i++) {
+document.write(nombre+ " x " +i+ " = "+nombre*i+"<br>");
+} 
