7599fc5fb444c0b0d3e12e7200f6fa4a08574a5b
AfpaYoussef <youssefchaibi@hotmail.com>
2022-10-21 12:25:04 +0000

    exercice fait

--- FRONT/DYNAMIQUE/JAVASCRIPT/assets/js/tableauexercice2.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/assets/js/tableauexercice2.js
@@ -2,12 +2,22 @@
 let valtab;
 let nomposte;
 let tab1 = new Array();
+let rechval;
+let max = tab1[tab1.length - 1];
+let min = tab1[0];
+let i;
+let j;
+let sum = 0;
+let compteur = 0;
+let moyenne = 0;
+
 
 function Getinteger()
 {
     // valtab = prompt(Number(Math.round()));
-    if(valtab != parseInt(valtab)){
+    while(valtab != parseInt(valtab)){
         alert("Veuillez rentrer un entier");
+        valtab = prompt("Entrez le nombre de postes souhaité");
     }
 }
 
@@ -15,32 +25,53 @@ function InitTab()
 {  
     valtab = Number(prompt("Entrez le nombre de postes souhaité"));
     console.log(valtab);
-    //Getinteger();
-    tab1 = Array(valtab); // entre parenthèses, le tableau comprendra n valeurs saisies et définies dans "valtab"
+    Getinteger();
+    tab1 = Array(valtab); // entre parenthèses, la longueur du tableau se définira par n valeurs saisies et définies dans "valtab"
     console.table(tab1);
 }
 
 function SaisieTab()
 {
     InitTab();
-    nomposte = prompt("Entrez les noms des postes: ");
-    console.log(nomposte);
+    for(i=0;i<valtab; i++) {
+        nomposte =  Number(prompt("Entrez le nombre "+(i+1)+" du tableau"));
+        tab1[i] = [nomposte];
+    }
+        AfficheTab();
 }
 
 function AfficheTab()
 {
-    SaisieTab();
-
+    console.table(tab1);
+    RechercheTab();
 }
 
 function RechercheTab()
-
-{
-    AfficheTab()
+{   
+    rechval = prompt("saisir le rang recherché du tableau:");
+    alert(tab1[rechval]);
+    InfoTab ();
 }
 
-function InfoTab ()
+
+function Somme()
+{
+    for (j = 0; j<valtab;j++) {
+        sum +=  parseInt(tab1[j]);
+        compteur++;
+    }
+}
+
+function Moyenne()
+{
+    Somme();
+    moyenne = sum / compteur;
+}
+
+function InfoTab()
 
 {
-    RechercheTab()
+    Moyenne();
+    Somme();
+    alert("le maximum des postes est " + tab1[tab1.length - 1] + " et la moyenne des postes est " + moyenne);
 }
