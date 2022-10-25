6defb07ac01ec614d614282c65a71be72cc883a9
AfpaYoussef <youssefchaibi@hotmail.com>
2022-10-25 16:53:02 +0000

    Enregistrement

---
+++ FRONT/DYNAMIQUE/JAVASCRIPT/assets/js/exercicejs15a.js
@@ -0,0 +1,7 @@
+let reponse;
+
+document.getElementById("button1").addEventListener("click",() => {
+reponse = document.getElementById("textBox1").value;
+alert("Vous avez écrit " + reponse);
+});
+
---
+++ FRONT/DYNAMIQUE/JAVASCRIPT/assets/js/exercicejs15b.js
@@ -0,0 +1,32 @@
+var alea=Math.round(Math.random()*99)+1;
+var coup=0;
+function test(valeur)
+{
+coup=coup+1;
+if (coup > 6)
+    {
+    partie=confirm("T'as perdu !\nIl fallait trouver : " + alea + "\nNouvelle partie ?");
+    if (partie)
+        {
+        document.form1.commentaire.value="Nouvelle partie...";
+        coup=0;
+        alea=Math.round(Math.random()*99)+1;
+        document.form1.coup.value=0;
+        valeur.value="";
+        }
+        else
+        {
+        window.close();
+        }
+    }
+    else
+    {
+    if (valeur.value < alea)
+        document.form1.commentaire.value="Trop petit !";
+    if (valeur.value > alea)
+        document.form1.commentaire.value="Trop grand !" ;
+    if (valeur.value == alea)
+        document.form1.commentaire.value="Gagné !!!";
+    document.form1.coup.value=coup;
+    }
+}
