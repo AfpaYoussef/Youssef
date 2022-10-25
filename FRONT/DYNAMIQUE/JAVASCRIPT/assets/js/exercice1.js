32c2dc7e8bae33b245e0bcd310ed2044808af925
AfpaYoussef <youssefchaibi@hotmail.com>
2022-10-25 14:15:30 +0000

    js14 effectué

---
+++ FRONT/DYNAMIQUE/JAVASCRIPT/assets/js/exercice1.js
@@ -0,0 +1,7 @@
+document.write("<h1>Mon 1er script</h1>")  
+
+// la fonction alerte ne prend pas en charge les balises html
+
+alert("Rien à ajouter"); 
+        console.log("Ah que Coucou !");
+        document.write("Coucou beuh !");
