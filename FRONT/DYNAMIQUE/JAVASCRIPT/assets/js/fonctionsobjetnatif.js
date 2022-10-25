31f63eab99c28fea9e7a492c1d9f6fce2d761fb9
AfpaYoussef <youssefchaibi@hotmail.com>
2022-10-25 11:48:51 +0000

    exercices objets natifs terminés

--- FRONT/DYNAMIQUE/JAVASCRIPT/assets/js/fonctionsobjetnatif.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/assets/js/fonctionsobjetnatif.js
@@ -1,21 +1,33 @@
+// Attention rafraichir entre deux utilisations du programme
+
 let tab1 = new Array();
 let valtab;
 let i=0;
+let j=0;
+let sum;
+let compteur;
+let moyenne;
 
 
-
-function Saisieval() {
     valtab = parseInt(window.prompt("Veuillez saisir plusieurs valeurs numériques:"));
     tab1[i]=valtab;
-    console.log(tab1);
     while (tab1[i]!=0) {
         valtab = parseInt(window.prompt("Encore une valeur numérique:"));
         i++;
+        compteur++;
         tab1[i]=valtab;
     }
-}
+
+    console.table(tab1);
+    const add = tab1.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+ 
+
+    moyenne = add / i;
 
 
- Saisieval ();
+    tab1.length = tab1.length-1;
+    
+
+    alert("Le nombre de valeurs saisies est de " + tab1.length +", sa somme de " +add +" et sa moyenne de " + moyenne)
 
 
